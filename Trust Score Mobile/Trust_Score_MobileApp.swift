9d0cf58d4c91f748d1f452b1e8267235030f1748
Jonathon Poe <noblesite@gmail.com>
2025-04-14 20:30:40 -0500

    Initial Commit

---
+++ Trust Score Mobile/Trust_Score_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trust_Score_MobileApp.swift
+//  Trust Score Mobile
+//
+//  Created by JONATHON POE on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Trust_Score_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Trust Score MobileTests/Trust_Score_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trust_Score_MobileTests.swift
+//  Trust Score MobileTests
+//
+//  Created by JONATHON POE on 4/14/25.
+//
+
+import Testing
+@testable import Trust_Score_Mobile
+
+struct Trust_Score_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
